4453e9c56c562c137279ef47f114cf02873f1ef7
STEPHENMUCHANGA <149421335+STEPHENMUCHANGA@users.noreply.github.com>
2025-07-02 16:11:25 +0300

    Update settings.py

--- alx-travel-app/settings.py
+++ alx-travel-app/settings.py
@@ -1,8 +1,4 @@
-## alx-travel-app
-### Django Project Setup with API Documentation and Database Configuration
-### Objective: Set up the Django project with the necessary dependencies, configure the database, and add Swagger for API documentation.
-## Settings
-### ```python
+```python
 import os
 import environ
 from pathlib import Path
