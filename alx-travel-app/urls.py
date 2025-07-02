58b3937361257a3e64f100c2950762dd0dcd9d91
STEPHENMUCHANGA <149421335+STEPHENMUCHANGA@users.noreply.github.com>
2025-07-02 16:11:59 +0300

    Update urls.py

--- alx-travel-app/urls.py
+++ alx-travel-app/urls.py
@@ -1,7 +1,3 @@
-## alx-travel-app
-### Django Project Setup with API Documentation and Database Configuration
-### Objective: Set up the Django project with the necessary dependencies, configure the database, and add Swagger for API documentation.
-## urls
 ```python
 from django.contrib import admin
 from django.urls import path, include
