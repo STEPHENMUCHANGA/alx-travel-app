e700dc97e606bf74753b8a6fbca86122e9367a59
STEPHENMUCHANGA <149421335+STEPHENMUCHANGA@users.noreply.github.com>
2025-07-02 15:32:04 +0300

    Update urls.py

--- alx-travel-app/urls.py
+++ alx-travel-app/urls.py
@@ -1,1 +1,26 @@
+## alx-travel-app
+### Django Project Setup with API Documentation and Database Configuration
+### Objective: Set up the Django project with the necessary dependencies, configure the database, and add Swagger for API documentation.
+## urls
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ALX Travel App API",
+        default_version='v1',
+        description="API documentation for the ALX Travel App",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('listings.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
 
