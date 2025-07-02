b31bbcc390c27acc73d65945ead8b2a531b1faf1
STEPHENMUCHANGA <149421335+STEPHENMUCHANGA@users.noreply.github.com>
2025-07-02 16:09:55 +0300

    Update celery.py

--- alx-travel-app/celery.py
+++ alx-travel-app/celery.py
@@ -1,7 +1,3 @@
-## alx-travel-app
-### Django Project Setup with API Documentation and Database Configuration
-### Objective: Set up the Django project with the necessary dependencies, configure the database, and add Swagger for API documentation.
-## celery
 ```python
 import os
 from celery import Celery
